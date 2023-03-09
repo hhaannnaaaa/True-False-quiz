a530f1f1970693a542ed309e0668f325c43bf129
hhaannnaaaa <hannalovisaberg@gmail.com>
2023-03-09 20:06:28 +0000

    Add div's in html for question and answers

---
+++ assets/css/js/script.js
@@ -0,0 +1,29 @@
+var questions = [
+    {
+        question: "Rihanna was born in the Bahamas.",
+        options: {"True":"False"},
+        answer: 2 
+    },
+    {
+        question: "The first music video to be played on MTV was Video Killed The Radio Star by The Buggles in 1981.",
+        options: {"True":"False"},
+        answer: 1 
+    },
+    {
+        question: "Before becoming queen, Queen Elizabeth II trained as a mechanic.",
+        options: {"True":"False"},
+        answer: 1 
+    },
+    {
+        question: "Victoria Beckham had a cameo appearance in the original Sex and the City series.",
+        options: {"True":"False"},
+        answer: 2 
+    },
+    {
+        question: "You can sneeze during your sleep.",
+        options: {"True":"False"},
+        answer: 2 
+    }
+]
+
+var score = 0;
